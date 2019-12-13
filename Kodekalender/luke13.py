be53f8163dae51183bddf98cffb3ae3a3ac9a811
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2019-12-13 08:43:09 -0800

    endelig

--- Kodekalender/luke13.py
+++ Kodekalender/luke13.py
@@ -21,39 +21,50 @@ def oppover(y,x):
         return False
     return True
 
-
 for y in range(len(maze)):
     for x in range(len(maze[y])):
         maze[y][x]["been"] = False
 
 
-stop = False
-room = json.loads(json.dumps(maze[0][0]))
-prevRoom = json.loads(json.dumps(room))
+
+maze[0][0]["been"] = True
+room = maze[0][0]
+prevRoom = room
 antallRom = 0
+run = True
+besokArr = [room]
 
 
-while (not stop):
-    prevRoom = json.loads(json.dumps(room))
-    [x, y] = [room["x"], room["y"]]
+while run:
+    x, y = room["x"], room["y"]
     if x==499 and y==499:
-        stop = True
-    if   not (maze[y+1][x]["been"])and (nedover(y, x)):
-        room = json.loads(json.dumps(maze[y+1][x]))
-    elif not (maze[y][x+1]["been"])and (hoyre(y, x)):
-        room = json.loads(json.dumps(maze[y][x+1]))
-    elif not (maze[y][x-1]["been"]) and (venstre(y, x)):
-        room = json.loads(json.dumps(maze[y][x-1]))
-    elif not (maze[y-1][x]["been"])and (oppover(y, x)):
-        room = json.loads(json.dumps(maze[y-1][x]))
-    else:
-        maze[y][x]["been"] = True
-        room = json.loads(json.dumps(prevRoom))
-    if not maze[y][x]["been"]:
+        run = False
+        
+    if   y < 499 and not maze[y+1][x]["been"] and (nedover(y, x)):
+        maze[y+1][x]["been"] = True
+        room = maze[y+1][x]
         antallRom+=1
-        maze[y][x]["been"] = True
-    print(room["x"], room["y"])
-print(antallRom)
+    elif x < 499 and not maze[y][x+1]["been"] and (hoyre(y, x)):
+        maze[y][x+1]["been"] = True
+        room = maze[y][x+1]
+        antallRom+=1
+
+    elif x > 0 and not maze[y][x-1]["been"] and (venstre(y, x)):
+        maze[y][x-1]["been"] = True
+        room = maze[y][x-1]
+        antallRom+=1
+
+    elif y > 0 and not maze[y-1][x]["been"] and (oppover(y, x)):
+        maze[y-1][x]["been"] = True
+        room = maze[y-1][x]
+        antallRom+=1
+    else:
+        room = besokArr[besokArr.index(room)-1]
+    besokArr.append(room)
+
+print(antallRom, len(besokArr))
+
+
 
     
 
