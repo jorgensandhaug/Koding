85566756a5ce507c6faca27a731d47224a624a18
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2019-12-16 11:57:45 -0800

    Spoill

---
+++ Spill som funker/Assets/TimeManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+   public float slowDownAmount = 0.05f;
+   public float slowDownLength = 1f;
+
+   void Update()
+   {
+       Time.timeScale += (1f/slowDownLength) * Time.unscaledDeltaTime;
+       Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+       Time.fixedDeltaTime = 0.02f*Time.timeScale;
+   }
+   public void SlowDown()
+   {
+       Time.timeScale = slowDownAmount;
+       Time.fixedDeltaTime = 0.02f*Time.timeScale;
+   }
+}
